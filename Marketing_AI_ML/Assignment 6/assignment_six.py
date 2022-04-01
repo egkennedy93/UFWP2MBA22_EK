b3134ab7d49ba8acd1bbd748b75c5d76eea84c7a
Everett Kennedy <egkennedy93@gmail.com>
2022-04-01 16:46:51 -0400

    final touches, complete

--- Marketing_AI_ML/Assignment 6/assignment_six.py
+++ Marketing_AI_ML/Assignment 6/assignment_six.py
@@ -33,11 +33,14 @@ kmeans.fit(scaled_features)
 data_with_clusters = data.copy()
 data_with_clusters['Clusters'] = kmeans.labels_
 
+
+
 # defining clusters into 4 sepearte DFs
 c0 = data_with_clusters.loc[data_with_clusters['Clusters'] == 0]
 c1 = data_with_clusters.loc[data_with_clusters['Clusters'] == 1]
 c2 = data_with_clusters.loc[data_with_clusters['Clusters'] == 2]
 c3 = data_with_clusters.loc[data_with_clusters['Clusters'] == 3]
+
 
 def elbow_chart():
     visualizer = KElbowVisualizer(KMeans(), k=(1,15)).fit(scaled_features)
@@ -50,19 +53,19 @@ def age_chart():
     #building the cluster labels
     c0_min = min(c0['Age'])
     c0_max = max(c0['Age'])
-    c0_range = ('{} - {}'.format(c0_min, c0_max))
+    c0_range = ('{} - {} C0'.format(c0_min, c0_max))
 
     c1_min = min(c1['Age'])
     c1_max = max(c1['Age'])
-    c1_range = ('{} - {}'.format(c1_min, c1_max))
+    c1_range = ('{} - {} C1'.format(c1_min, c1_max))
 
     c2_min = min(c2['Age'])
     c2_max = max(c2['Age'])
-    c2_range = ('{} - {}'.format(c2_min, c2_max))
+    c2_range = ('{} - {} C2'.format(c2_min, c2_max))
 
     c3_min = min(c3['Age'])
     c3_max = max(c3['Age'])
-    c3_range = ('{} - {}'.format(c3_min, c3_max))
+    c3_range = ('{} - {} C3'.format(c3_min, c3_max))
 
     # the labels would normally have the cluster IDs. i'm replacing them with the cluster ranges above
     data_with_clusters['Cluster Values'] = data_with_clusters['Clusters'].replace([0,1,2,3], [c0_range, c1_range, c2_range, c3_range])
@@ -77,19 +80,19 @@ def Annual_Income_chart():
     #building the cluster labels
     c0_min = min(c0['Annual Income (k$)'])
     c0_max = max(c0['Annual Income (k$)'])
-    c0_range = ('{} - {}'.format(c0_min, c0_max))
+    c0_range = ('{} - {} C0'.format(c0_min, c0_max))
 
     c1_min = min(c1['Annual Income (k$)'])
     c1_max = max(c1['Annual Income (k$)'])
-    c1_range = ('{} - {}'.format(c1_min, c1_max))
+    c1_range = ('{} - {} C1'.format(c1_min, c1_max))
 
     c2_min = min(c2['Annual Income (k$)'])
     c2_max = max(c2['Annual Income (k$)'])
-    c2_range = ('{} - {}'.format(c2_min, c2_max))
+    c2_range = ('{} - {} C2'.format(c2_min, c2_max))
 
     c3_min = min(c3['Annual Income (k$)'])
     c3_max = max(c3['Annual Income (k$)'])
-    c3_range = ('{} - {}'.format(c3_min, c3_max))
+    c3_range = ('{} - {} C3'.format(c3_min, c3_max))
 
     # the labels would normally have the cluster IDs. i'm replacing them with the cluster ranges above
     data_with_clusters['Cluster Values'] = data_with_clusters['Clusters'].replace([0,1,2,3], [c0_range, c1_range, c2_range, c3_range])
@@ -105,19 +108,19 @@ def Spending_Score_chart():
     #building the cluster labels
     c0_min = min(c0['Spending Score (1-100)'])
     c0_max = max(c0['Spending Score (1-100)'])
-    c0_range = ('{} - {}'.format(c0_min, c0_max))
+    c0_range = ('{} - {} C0'.format(c0_min, c0_max))
 
     c1_min = min(c1['Spending Score (1-100)'])
     c1_max = max(c1['Spending Score (1-100)'])
-    c1_range = ('{} - {}'.format(c1_min, c1_max))
+    c1_range = ('{} - {} C1'.format(c1_min, c1_max))
 
     c2_min = min(c2['Spending Score (1-100)'])
     c2_max = max(c2['Spending Score (1-100)'])
-    c2_range = ('{} - {}'.format(c2_min, c2_max))
+    c2_range = ('{} - {} C2'.format(c2_min, c2_max))
 
     c3_min = min(c3['Spending Score (1-100)'])
     c3_max = max(c3['Spending Score (1-100)'])
-    c3_range = ('{} - {}'.format(c3_min, c3_max))
+    c3_range = ('{} - {} C3'.format(c3_min, c3_max))
 
     # the labels would normally have the cluster IDs. i'm replacing them with the cluster ranges above
     data_with_clusters['Cluster Values'] = data_with_clusters['Clusters'].replace([0,1,2,3], [c0_range, c1_range, c2_range, c3_range])
@@ -130,9 +133,38 @@ def Spending_Score_chart():
 
 def overall_data():
     #since for the project I selected Cluster 3 for analysis, i'm doing analysis for that cluster.
+    #linear regression plots
+    df = c3
+
+    sns.lmplot(x='Annual Income (k$)', y='Spending Score (1-100)', data = df)
+    sns.lmplot(x='Age', y='Spending Score (1-100)', data = df)
+    sns.lmplot(x='Age', y='Annual Income (k$)', data = df)
+    print("\n")
+    plt.show()
+
+    x = df['Age']
+    y = df['Annual Income (k$)']
+    z = df['Spending Score (1-100)']
+
+    #performing different correlation analysis 
+    income_spend_corr = y.corr(z)
+    age_spend_corr = x.corr(z)
+    age_income_corr = x.corr(y)
+    correlation = ' Correlation: \nincome_spending correlation: {} \nage_spending correlation: {}\nage_income correlation: {}\n'.format(income_spend_corr, age_spend_corr, age_income_corr)
+    print(correlation)
+    print("\n")
+
+    #performing different means of each attribute
+    age_mean = df['Age'].mean()
+    income_mean = df['Annual Income (k$)'].mean()
+    spend_mean = df['Spending Score (1-100)'].mean()
+    means = ' means: \nage mean: {} \nspend mean: {} \nincome mean: {}'.format(age_mean, spend_mean, income_mean)
+    print(means)
+    print("\n")
 
     #ols can't handle non-alphanumeric characters
     c3.rename(columns={'Spending Score (1-100)': "Spend", 'Annual Income (k$)':'Income'}, inplace=True)
+    #performing ANOVA
     lm = ols('Spend ~ Gender + Age + Income', data=c3).fit()
     print('Male & Female stats')
     print(lm.summary())
@@ -145,12 +177,15 @@ def gender_data(gender_id):
         if gender_id == value:
             gender_name = key
 
+    
     #only selecting values that match the gender ID from the cluster selected
     df = c3.loc[c3['Gender'] == gender_id]
+    # print(df)
     #linear regression plots
     sns.lmplot(x='Annual Income (k$)', y='Spending Score (1-100)', data = df)
     sns.lmplot(x='Age', y='Spending Score (1-100)', data = df)
     sns.lmplot(x='Age', y='Annual Income (k$)', data = df)
+    plt.show()
 
     x = df['Age']
     y = df['Annual Income (k$)']
@@ -173,7 +208,6 @@ def gender_data(gender_id):
     #ANOVA 
     df.rename(columns={'Spending Score (1-100)': "Spend", 'Annual Income (k$)':'Income'}, inplace=True)
     lm = ols('Spend ~ Age + Income', data=df).fit()
-    table = sm.stats.anova_lm(lm)
     print(lm.summary())
 
 if __name__ == "__main__":
@@ -181,6 +215,8 @@ if __name__ == "__main__":
     # Annual_Income_chart()
     # Spending_Score_chart()
     # elbow_chart()
-    # gender_data(1)
-    # gender_data(2)
-    overall_data()
+    
+    # overall_data()
+    # male = gender_data(1)
+    # female = gender_data(2)
+    
--- Marketing_AI_ML/Assignment 6/notebook_assignment_six.py
+++ Marketing_AI_ML/Assignment 6/notebook_assignment_six.py
@@ -2,55 +2,115 @@
 # %%
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from yellowbrick.cluster import KElbowVisualizer
-import seaborn as sns
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+import os 
+import seaborn as sns; sns.set(style="ticks", color_codes=True)
+from scipy.stats import ttest_1samp, f_oneway, ttest_ind
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
+current_dir = os.getcwd()
+
+# defining the file and dropping genre since pandas doesn't like strings
 file_name = "Mall_Customers.csv"
-df = pd.read_csv(file_name)
+df = pd.read_csv(file_name).rename(columns={'Genre': 'Gender'})
+gender_cat = {'Male': 1, 'Female': 2}
+# data = df.drop(columns=['Genre'])
+df['Gender'] = df['Gender'].map(gender_cat)
+data = df
 
-data = df.drop(columns=['Genre'])
-
+#using a scaler to standardize the data
 scaler = StandardScaler()
 scaled_features = scaler.fit_transform(data)
 
-visualizer = KElbowVisualizer(KMeans(), k=(1,15)).fit(scaled_features)
-plt.legend(loc=2, bbox_to_anchor= (1,1))
-visualizer.show()
-#%%
+#After looking at the elbow_chart function below, determined n_clusters to be 4. Now fitting the scaled_features to
+# a 4 seed cluster
 kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=15, random_state=0)
 kmeans.fit(scaled_features)
 
-scaled_features
-
+#copying the fitted data to a new df, and adding the clusters column 
 data_with_clusters = data.copy()
 data_with_clusters['Clusters'] = kmeans.labels_
-#%%
+
+# defining clusters into 4 sepearte DFs
 c0 = data_with_clusters.loc[data_with_clusters['Clusters'] == 0]
 c1 = data_with_clusters.loc[data_with_clusters['Clusters'] == 1]
 c2 = data_with_clusters.loc[data_with_clusters['Clusters'] == 2]
 c3 = data_with_clusters.loc[data_with_clusters['Clusters'] == 3]
+c3
+#%%
+sns.lmplot(x='Annual Income (k$)', y='Spending Score (1-100)', data = c3)
+sns.lmplot(x='Age', y='Spending Score (1-100)', data = c3)
+sns.lmplot(x='Age', y='Annual Income (k$)', data = c3)
 
-c0_min = min(c0['Age'])
-c0_max = max(c0['Age'])
-c0_range = ('{} - {}'.format(c0_min, c0_max))
+x = c3['Age']
+y = c3['Annual Income (k$)']
+z = c3['Spending Score (1-100)']
 
-c1_min = min(c1['Age'])
-c1_max = max(c1['Age'])
-c1_range = ('{} - {}'.format(c1_min, c1_max))
+age_income_corr = x.corr(y)
+age_spend_corr = x.corr(z)
+income_spend_corr = y.corr(z)
+#%%
+male_df = c3.loc[c3['Gender'] == 1]
+male_df
 
-c2_min = min(c2['Age'])
-c2_max = max(c2['Age'])
-c2_range = ('{} - {}'.format(c2_min, c2_max))
+sns.lmplot(x='Annual Income (k$)', y='Spending Score (1-100)', data = male_df)
+sns.lmplot(x='Age', y='Spending Score (1-100)', data = male_df)
+sns.lmplot(x='Age', y='Annual Income (k$)', data = male_df)
 
-c3_min = min(c3['Age'])
-c3_max = max(c3['Age'])
-c3_range = ('{} - {}'.format(c3_min, c3_max))
+m_x = male_df['Age']
+m_y = male_df['Annual Income (k$)']
+m_z = male_df['Spending Score (1-100)']
 
-data_with_clusters['Clusters'] = data_with_clusters['Clusters'].replace([0,1,2,3], [c0_range, c1_range, c2_range, c3_range])
-box = sns.boxplot(x=data_with_clusters['Clusters'], y=data_with_clusters['Age'], palette="Set3")
+m_income_spend_corr = y.corr(z)
+m_age_spend_corr = x.corr(z)
+m_age_income_corr = x.corr(y)
+
+m_age_mean = male_df['Age'].mean()
+m_spend_mean = male_df['Annual Income (k$)'].mean()
+m_income_mean = male_df['Spending Score (1-100)'].mean()
+
 # %%
-kmeans.labels_
+female_df = c3.loc[c3['Gender'] == 2]
+sns.lmplot(x='Annual Income (k$)', y='Spending Score (1-100)', data = female_df)
+sns.lmplot(x='Age', y='Spending Score (1-100)', data = female_df)
+sns.lmplot(x='Age', y='Annual Income (k$)', data = female_df)
+
+f_x = female_df['Age']
+f_y = female_df['Annual Income (k$)']
+f_z = female_df['Spending Score (1-100)']
+
+f_income_spend_corr = y.corr(z)
+f_age_spend_corr = x.corr(z)
+f_age_income_corr = x.corr(y)
+
+f_age_mean = male_df['Age'].mean()
+f_spend_mean = male_df['Annual Income (k$)'].mean()
+f_income_mean = male_df['Spending Score (1-100)'].mean()
+
+ 
+#%%
+test = ttest_1samp(female_df['Age'], b=female_df['Spending Score (1-100)'].mean(), equal_var=True)
+test
+# %%
+df = data.rename(columns={'Spending Score (1-100)': "Spend", 'Annual Income (k$)':'Income'}, inplace=True)
+# %%
+lm = ols('Spend ~ Gender + Age + Income', data=c3).fit()
+table = sm.stats.anova_lm(lm)
+# %%
+print(lm.summary())
+# %%
+male_df.rename(columns={'Spending Score (1-100)': "Spend", 'Annual Income (k$)':'Income'}, inplace=True)
+lm = ols('Spend ~ Age + Income', data=male_df).fit()
+table = sm.stats.anova_lm(lm)
+print(lm.summary())
+# %%
+female_df.rename(columns={'Spending Score (1-100)': "Spend", 'Annual Income (k$)':'Income'}, inplace=True)
+lm = ols('Spend ~ Age + Income', data=female_df).fit()
+table = sm.stats.anova_lm(lm)
+print(lm.summary())
 # %%
