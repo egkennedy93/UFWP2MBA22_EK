05d7887698a31a9469925b0b0a906a3ea938638f
Everett Kennedy <egkennedy93@gmail.com>
2022-10-18 14:29:17 -0400

    Initial release

---
+++ FINTECH/CountryRisk_2019_kmeans_results.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# loading packages
+
+import os
+
+import pandas as pd
+import numpy as np
+
+# plotting packages
+#get_ipython().run_line_magic('matplotlib', 'inline')
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as clrs
+
+# Kmeans algorithm from scikit-learn
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+
+# ## Load raw data
+
+# In[2]:
+
+
+# load raw data
+DATA_FOLDER ='/home/egkennedy93/programming_projects/UFWP2MBA22_EK/FINTECH/'
+raw = pd.read_csv(os.path.join(DATA_FOLDER, 'country risk 2019 data.csv'))
+
+# check the raw data
+print("Size of the dataset (row, col): ", raw.shape)
+print("\nFirst 5 rows\n", raw.head(n=5))
+
+
+# ## Simple exploratory analysis
+# ### Print summary statistics
+# Note that all features have quite different variances, and Corruption and Legal are highly correlated.
+
+# In[3]:
+
+
+# print summary statistics
+print("\nSummary statistics\n", raw.describe())
+print("\nCorrelation matrix\n", raw.corr())
+
+
+# ### Plot histogram
+# 
+# Note that distributions for GDP Growth is quite skewed.
+
+# In[4]:
+
+
+# plot histograms
+plt.figure(1)
+raw['Corruption'].plot(kind = 'hist', title = 'Corruption', alpha = 0.5)
+
+plt.figure(2)
+raw['Peace'].plot(kind = 'hist', title = 'Peace', alpha = 0.5)
+
+plt.figure(3)
+raw['Legal'].plot(kind = 'hist', title = 'Legal', alpha = 0.5)
+
+plt.figure(4)
+raw['GDP Growth'].plot(kind = 'hist', title = 'GDP Growth', alpha = 0.5)
+
+plt.show()
+
+
+# ## K means cluster
+# ### Pick features & normalization
+# 
+# Since Corruption and Legal are highly correlated, we drop the Courrption variable, i.e., we pick three features for this analysis, Peace, Legal and GDP Grwoth. Let's normalize all the features, effectively making them equally weighted.
+# 
+# Ref. [Feature normalization.](https://stats.stackexchange.com/questions/21222/are-mean-normalization-and-feature-scaling-needed-for-k-means-clustering)
+
+# In[5]:
+
+
+X = raw[['Peace', 'Legal', 'GDP Growth']]
+X = (X - X.mean()) / X.std()
+print(X.head(5))
+
+
+# ### Perform elbow method
+# 
+# The marginal gain of adding one cluster dropped quite a bit from k=3 to k=4. We will choose k=3 (not a clear cut though).
+# 
+# Ref. [Determining the number of clusters in a dataset.](https://en.wikipedia.org/wiki/Determining_the_number_of_clusters_in_a_data_set)
+
+# In[6]:
+
+
+# https://stackoverflow.com/questions/41540751/sklearn-kmeans-equivalent-of-elbow-method
+
+Ks = range(1, 10)
+inertia = [KMeans(i).fit(X).inertia_ for i in Ks]
+
+fig = plt.figure()
+plt.plot(Ks, inertia, '-bo')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia (within-cluster sum of squares)')
+plt.show()
+
+
+# ### K means with k=3
+
+# In[7]:
+
+
+k = 3
+kmeans = KMeans(n_clusters=k, random_state=1)
+kmeans.fit(X)
+
+# print inertia & cluster center
+print("inertia for k=3 is", kmeans.inertia_)
+print("cluster centers: ", kmeans.cluster_centers_)
+
+# take a quick look at the result
+y = kmeans.labels_
+print("cluster labels: ", y)
+
+
+# ### Visualize the result (3D plot)
+
+# In[8]:
+
+
+# set up the color
+norm = clrs.Normalize(vmin=0.,vmax=y.max() + 0.8)
+cmap = cm.viridis
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(X.iloc[:,0], X.iloc[:,1], X.iloc[:,2], c=cmap(norm(y)), marker='o')
+
+centers = kmeans.cluster_centers_
+ax.scatter(centers[:, 0], centers[:, 1], c='black', s=100, alpha=0.5)
+
+ax.set_xlabel('Peace')
+ax.set_ylabel('Legal')
+ax.set_zlabel('GDP Growth')
+
+plt.show()
+
+
+# ### Visualize the result (3 2D plots)
+
+# In[9]:
+
+
+#get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+
+figs = [(0, 1), (0, 2), (1, 2)]
+labels = ['Peace', 'Legal', 'GDP Growth']
+
+for i in range(3):
+    fig = plt.figure(i)
+    plt.scatter(X.iloc[:,figs[i][0]], X.iloc[:,figs[i][1]], c=cmap(norm(y)), s=50)
+    plt.scatter(centers[:, figs[i][0]], centers[:, figs[i][1]], c='black', s=200, alpha=0.5)
+    plt.xlabel(labels[figs[i][0]])
+    plt.ylabel(labels[figs[i][1]])
+
+plt.show()
+
+
+# ### Visualize the result (3 2D plots)
+# plot country abbreviations instead of dots.
+
+# In[10]:
+
+
+#get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+
+figs = [(0, 1), (0, 2), (1, 2)]
+labels = ['Peace', 'Legal', 'GDP Growth']
+colors = ['blue','green', 'red']
+
+for i in range(3):
+    fig = plt.figure(i, figsize=(8, 8))
+    x_1 = figs[i][0]
+    x_2 = figs[i][1]
+    plt.scatter(X.iloc[:, x_1], X.iloc[:, x_2], c=y, s=0, alpha=0)
+    plt.scatter(centers[:, x_1], centers[:, x_2], c='black', s=200, alpha=0.5)
+    for j in range(X.shape[0]):
+        plt.text(X.iloc[j, x_1], X.iloc[j, x_2], raw['Abbrev'].iloc[j], 
+                 color=colors[y[j]], weight='semibold', horizontalalignment = 'center', verticalalignment = 'center')
+    plt.xlabel(labels[x_1])
+    plt.ylabel(labels[x_2])
+
+plt.show()
+
+
+# ### List the result
+
+# In[11]:
+
+
+result = pd.DataFrame({'Country':raw['Country'], 'Abbrev':raw['Abbrev'], 'Label':y})
+with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
+    print(result.sort_values('Label'))
+
+
+# In[12]:
+
+
+# Silhouette Analysis
+range_n_clusters=[2,3,4,5,6,7,8,9,10]
+for n_clusters in range_n_clusters:
+    clusterer=KMeans(n_clusters=n_clusters, random_state=1)
+    cluster_labels=clusterer.fit_predict(X)
+    silhouette_avg=silhouette_score(X,cluster_labels)
+    print("For n_clusters=", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
+
+
+# In[ ]:
+
+
+
+
